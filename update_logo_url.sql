0c9f8fefb7315a9c55d37833fd6a10be4fc27f06
Ankit <ankit.pandeyiitmbs@gmail.com>
2026-04-23 15:22:33 +0530

    Update logo URL in SQL script

--- update_logo_url.sql
+++ update_logo_url.sql
@@ -1,4 +1,4 @@
 -- Update site settings with new logo URL
 UPDATE public.site_settings
-SET logo_url = 'https://i.ibb.co/QjY2c5Hm/T-1.png'
+SET logo_url = 'https://i.ibb.co/S48cvCy6/T-1-1.png'
 WHERE id = 'global';
